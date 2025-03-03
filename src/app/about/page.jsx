c2ee3abd92108dd9c12ff463f693cb03362c4d76
BadhonDev <badhon@ebiw.com>
2025-03-03 13:21:23 +0600

    footer changed

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import Container from "@/components/Container"
+import Lamp from "@/components/Lamp"
+import { Button } from "@/components/ui/button"
+
+export default function About() {
+  return (
+    <div className="">
+      <Lamp />
+    </div>
+  )
+}
