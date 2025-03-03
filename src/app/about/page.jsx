3bfd360f202366f0ed79d4d25194937b07f5563f
Shoishob007 <u1704049@student.cuet.ac.bd>
2025-03-03 23:44:52 +0600

    fixed git issue (2)

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,15 @@
+import { BackgroundBoxesDemo } from "@/components/BackgroundBox"
 import Container from "@/components/Container"
 import Lamp from "@/components/Lamp"
+import AboutSection from "@/components/AboutSection"
 import { Button } from "@/components/ui/button"
 
 export default function About() {
   return (
     <div className="">
-      <Lamp />
+      {/* <Lamp /> */}
+      <BackgroundBoxesDemo title={"About"}/>
+      <AboutSection/>
     </div>
   )
 }
---
+++ src/app/product/page.jsx
@@ -0,0 +1,12 @@
+import { BackgroundBoxesDemo } from "@/components/BackgroundBox"
+import { Button } from "@/components/ui/button"
+
+export default function Product() {
+  return (
+    <div className="">
+      {/* <Lamp /> */}
+      <BackgroundBoxesDemo title={"Product"}/>
+      <ProductSection />
+    </div>
+  )
+}
